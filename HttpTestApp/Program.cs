e97655adb4662d600effefa18b6497ad40e77cad
hbb0b0 <hbb0b0@163.com>
2021-03-26 16:05:11 +0800

    update

---
+++ HttpTestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://wx.koudl.cn/";
+
+          var data=  PostHelper.PostWebRequest(url, "", Encoding.Unicode);
+            Console.Write(data);
+        }
+    }
+}
