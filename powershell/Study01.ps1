a3798fcc1e6bbc5b7f167f1e7eb4ec42c951728a
hbb0b0 <hbb0b0@163.com>
2021-03-30 18:06:38 +0800

    update

--- powershell/Study01.ps1
+++ powershell/Study01.ps1
@@ -1,6 +1,7 @@
 #查看当前 powershell的版本
 $PSVersionTable
 
+#Get-Help 是多用途命令。 Get-Help 帮助你了解找到命令后如何使用它们。 Get-Help 也可用于帮助查找命令，但与 Get-Command 相比，它采用不同且较为间接的方式
 #检查当前的执行策略
 Get-ExecutionPolicy
 
@@ -21,3 +22,5 @@ Get-Process OpenConsole
 
 #查看进程的文件信息
 Get-Process OpenConsole -FileVersionInfo
+
+#Get-Command 的作用是帮助查找命令。 运行不带任何参数的 Get-Command 会返回系统上所有命令的列表。 以下示例演示使用 Get-Command cmdlet 确定存在的用于处理进程的命令：
