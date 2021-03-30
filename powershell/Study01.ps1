5a36cff8568bfe8d46d2b0942d2a2098fc64dc3f
hbb0b0 <hbb0b0@163.com>
2021-03-30 17:52:06 +0800

    update

---
+++ powershell/Study01.ps1
@@ -0,0 +1,17 @@
+#查看当前 powershell的版本
+$PSVersionTable
+
+#检查当前的执行策略
+Get-ExecutionPolicy
+
+#获取帮助
+Get-Help -Name Get-Help
+
+#获取全部的帮助
+Get-Help -Name Get-Help -Full
+
+#Get-Help 示例
+#获取所有结尾为*process的命令
+get-help *process
+#获取 Get-Process 的帮助
+Get-help -name get-process
