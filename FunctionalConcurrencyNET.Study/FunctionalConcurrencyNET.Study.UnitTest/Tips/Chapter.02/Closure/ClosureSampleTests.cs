e360c835f98259232174fd30f707053438bba69b
hbb0b0 <hbb0b0@163.com>
2021-04-20 18:01:49 +0800

    update

---
+++ FunctionalConcurrencyNET.Study/FunctionalConcurrencyNET.Study.UnitTest/Tips/Chapter.02/Closure/ClosureSampleTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using FunctionalConcurrencyNET.Study.Tips.Chapter._02.Closure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalConcurrencyNET.Study.Tips.Chapter._02.Closure.Tests
+{
+    public class ClosureSampleTests
+    {
+        [Fact()]
+        public void Closure_Strange_BehaviorTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+
+            ClosureSample closureSample = new ClosureSample();
+            IEnumerable<int> expectList = Enumerable.Range(0, 10);
+            IEnumerable<int> factList = closureSample.Closure_Strange_Behavior();
+            //factList.ForEach(p =>
+            //{
+            //    Console.WriteLine(p);
+
+            //});
+
+            
+            
+    }
+}
---
+++ FunctionalConcurrencyNET.Study/FunctionalConcurrencyNET.Study/Tips/Chapter.02/Closure/ClosureSample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunctionalConcurrencyNET.Study.Tips.Chapter._02.Closure
+{
+    public class ClosureSample
+    {
+        public IEnumerable<int> Closure_Strange_Behavior()
+        {
+            int iterations = 10;
+            string text = "";
+            List<int> resultList = new List<int>();
+            for (int i = 1; i <= iterations; i++)
+            {
+  
+                resultList.Add(i);
+                text = i.ToString();
+                Task.Factory.StartNew(() =>
+                    Console.WriteLine(text));
+            }
+            return resultList;
+        }
+
+        public void Closure_Correct_Behavior()
+        {
+            int iterations = 10;
+            for (int i = 1; i <= iterations; i++)
+            {
+                var index = i;
+                Task.Factory.StartNew(() =>
+                    Console.WriteLine("{0} - {1}", Thread.CurrentThread.ManagedThreadId, index));
+            }
+        }
+    }
+}
