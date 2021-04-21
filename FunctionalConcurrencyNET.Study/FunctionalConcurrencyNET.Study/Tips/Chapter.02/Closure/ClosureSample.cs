df712a354f4e41d9248f4218e5d7147128a1c61e
hbb0b0 <hbb0b0@163.com>
2021-04-21 18:06:06 +0800

    20210421

--- FunctionalConcurrencyNET.Study/FunctionalConcurrencyNET.Study/Tips/Chapter.02/Closure/ClosureSample.cs
+++ FunctionalConcurrencyNET.Study/FunctionalConcurrencyNET.Study/Tips/Chapter.02/Closure/ClosureSample.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
 
 namespace FunctionalConcurrencyNET.Study.Tips.Chapter._02.Closure
 {
     public class ClosureSample
     {
+      
         public IEnumerable<int> Closure_Strange_Behavior()
         {
             int iterations = 10;
@@ -16,11 +18,10 @@ namespace FunctionalConcurrencyNET.Study.Tips.Chapter._02.Closure
             List<int> resultList = new List<int>();
             for (int i = 1; i <= iterations; i++)
             {
-  
-                resultList.Add(i);
-                text = i.ToString();
                 Task.Factory.StartNew(() =>
-                    Console.WriteLine(text));
+                    Console.WriteLine("{0} - {1}", Thread.CurrentThread.ManagedThreadId, i));
+                Task.Factory.StartNew(() =>
+                    Console.WriteLine("{0} - {1}", Thread.CurrentThread.ManagedThreadId, i));
             }
             return resultList;
         }
@@ -35,5 +36,8 @@ namespace FunctionalConcurrencyNET.Study.Tips.Chapter._02.Closure
                     Console.WriteLine("{0} - {1}", Thread.CurrentThread.ManagedThreadId, index));
             }
         }
+
+        // Listing 2.5 Closure defined in C# using an anonymous method
+       
     }
 }
