42bad102f7dc5a113846671f5e26718249e73e19
hbb0b0 <hbb0b0@163.com>
2021-05-20 18:12:39 +0800

    update

--- AsyncStudy/AsyncFunctionCodeSample/Program.cs
+++ AsyncStudy/AsyncFunctionCodeSample/Program.cs
@@ -1,21 +1,25 @@
 ﻿using System;
-
+using AsyncFunctionCodeSample.Sample;
 namespace AsyncFunctionCodeSample
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
+
 
             //等待异步事件
             //IRun asyncVoidExample = new AsyncVoidExample();
 
             //asyncVoidExample.Run();
 
-            IRun IRun2 = new TaskCompletionSourceSample();
+            //IRun IRun2 = new TaskCompletionSourceSample();
 
-            IRun2.Run();
+            //IRun2.Run();
+
+            IRun IRun3 = new GloabalRandomWithNoLock();
+
+            IRun3.Run();
 
             Console.ReadLine();
         }
