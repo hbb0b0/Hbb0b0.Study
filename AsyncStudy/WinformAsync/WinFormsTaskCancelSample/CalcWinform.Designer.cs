f513ca9a007bacb60dd2e0ae82e70377093c6ac7
hbb0b0 <hbb0b0@163.com>
2021-05-24 17:41:38 +0800

    update

---
+++ AsyncStudy/WinformAsync/WinFormsTaskCancelSample/CalcWinform.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace WinFormsTaskCancelSample
+{
+    partial class CalcWinform
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(68, 169);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "开始";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(149, 169);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(68, 130);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(100, 23);
+            this.tbNumber.TabIndex = 2;
+            // 
+            // CalcWinform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRun);
+            this.Name = "CalcWinform";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox tbNumber;
+    }
+}
+
---
+++ AsyncStudy/WinformAsync/WinFormsTaskCancelSample/CalcWinform.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTaskCancelSample
+{
+    public partial class CalcWinform : Form
+    {
+        public CalcWinform()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRun_Click(object sender, EventArgs e)
+        {
+            int number = 0;
+             long result = 0;
+            if( int.TryParse( tbNumber.Text.Trim(),out number))
+            {
+               result =    await CalculatorFibnacci.FibAsync(number);
+
+                MessageBox.Show($"Number:{number} Result:{result}");
+            }
+        }
+    }
+}
